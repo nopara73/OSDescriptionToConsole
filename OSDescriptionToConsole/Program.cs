2278b3f4a572167b032e45b34d37d08429bf2ac3
nopara73 <adam.ficsor73@gmail.com>
2018-10-19 16:47:19 +0200

    Add project files.

---
+++ OSDescriptionToConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OSDescriptionToConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
