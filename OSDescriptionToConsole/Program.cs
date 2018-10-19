c2b588ae39a8312fb1cd6f2732a32a2144de70cd
nopara73 <adam.ficsor73@gmail.com>
2018-10-19 16:51:10 +0200

    Init project

--- OSDescriptionToConsole/Program.cs
+++ OSDescriptionToConsole/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace OSDescriptionToConsole
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"{nameof(RuntimeInformation.FrameworkDescription)}: {RuntimeInformation.FrameworkDescription}");
+            Console.WriteLine($"{nameof(RuntimeInformation.OSArchitecture)}: {RuntimeInformation.OSArchitecture}");
+            Console.WriteLine($"{nameof(RuntimeInformation.OSDescription)}: {RuntimeInformation.OSDescription}");
+            Console.WriteLine($"{nameof(RuntimeInformation.ProcessArchitecture)}: {RuntimeInformation.ProcessArchitecture}");
+            Console.WriteLine();
+            Console.WriteLine("Press a key to close...");
+            Console.ReadKey();
         }
     }
 }
